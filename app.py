c37a2eca025bd779a00067d972ac53903eb4cd81
aarzoomehta <mehtaaarzoo152@gmail.com>
2026-01-21 02:32:30 +0530

    Fix for deployment

--- app.py
+++ app.py
@@ -73,9 +73,12 @@ def home():
 
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=10000)
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
